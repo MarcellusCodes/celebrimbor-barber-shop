9da4572b2e59da812b41d6e4e6d74bfb0e7d6a8e
MarcellusCodes <MarcellusCodes@gmail.com>
2022-03-15 23:38:57 +0100

    Installed and Setup Theming, Fonts TailwindCSS, axios for api calls, created folder structure,

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2E2F36",
+        secondary: "#ADABB0",
+        tertiary: "#C1945E",
+      },
+      fontFamily: {
+        primary: ["Luckiest Guy"],
+        secondary: ["Yrsa"],
+      },
+    },
+  },
+  plugins: [],
+};
